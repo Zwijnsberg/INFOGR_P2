e0d14b2b4ab49b6fa4c65ded68cce1480b8fa54d
MennoBrl <51479931+MennoBrl@users.noreply.github.com>
2020-06-23 18:36:06 +0200

    allemaal scenegraph veranderingen

--- SceneGraph.cs
+++ SceneGraph.cs
@@ -4,6 +4,7 @@ using OpenTK;
 using System.Runtime.InteropServices;
 using OpenTK.Graphics.OpenGL;
 using Template;
+using System.Drawing.Drawing2D;
 
 // using meshes
 
@@ -14,19 +15,21 @@ namespace INFOGR2019Tmpl8
         public List<SceneGraph> children = new List<SceneGraph>();
         public Mesh me; 
         public Mesh parent;
+		public Vector3 pos;
+		public float rotation;
+		public Vector3 scale;
 
-		private Matrix4 transform;
-
-		public SceneGraph(Mesh Me, Matrix4 camera)
+		public SceneGraph(Mesh Me)
         {
             me = Me;
             parent = me.parent;
+			
 
             if (me.kids != null)
             {
                 for (int i = 0; i < me.kids.Length; i++)
                 {
-                    SceneGraph child = new SceneGraph(me.kids[i], camera);
+                    SceneGraph child = new SceneGraph(me.kids[i]);
                     children.Add(child);
                 }
             }
@@ -38,70 +41,14 @@ namespace INFOGR2019Tmpl8
 		// render the mesh using the supplied shader and matrix
 		public void Render(Matrix4 camera)
 		{
-			Mesh mesh = me;
-			Shader shader = mesh.shader;
-			Matrix4 transform = mesh.modelViewMatrix;
-			Matrix4 toWorld = mesh.toWorld;
-			Texture texture = mesh.texture;
-
-			//me.Render(shader, transform, toWorld, texture);
-
-			
-			// on first run, prepare buffers
-			me.Prepare(shader);
-
-			// safety dance
-			GL.PushClientAttrib(ClientAttribMask.ClientVertexArrayBit);
-
-			// enable texture
-			int texLoc = GL.GetUniformLocation(shader.programID, "pixels");
-			GL.Uniform1(texLoc, 0);
-			GL.ActiveTexture(TextureUnit.Texture0);
-			GL.BindTexture(TextureTarget.Texture2D, texture.id);
-
-			// enable shader
-			GL.UseProgram(shader.programID);
-
-			// pass transform to vertex shader
-			GL.UniformMatrix4(shader.uniform_mview, false, ref transform);
-			GL.UniformMatrix4(shader.uniform_2wrld, false, ref toWorld);
-
-			// enable position, normal and uv attributes
-			GL.EnableVertexAttribArray(shader.attribute_vpos);
-			GL.EnableVertexAttribArray(shader.attribute_vnrm);
-			GL.EnableVertexAttribArray(shader.attribute_vuvs);
-
-			// bind interleaved vertex data
-			GL.EnableClientState(ArrayCap.VertexArray);
-			GL.BindBuffer(BufferTarget.ArrayBuffer, me.vertexBufferId);
-			GL.InterleavedArrays(InterleavedArrayFormat.T2fN3fV3f, Marshal.SizeOf(typeof(ObjVertex)), IntPtr.Zero);
-
-			// link vertex attributes to shader parameters 
-			GL.VertexAttribPointer(shader.attribute_vuvs, 2, VertexAttribPointerType.Float, false, 32, 0);
-			GL.VertexAttribPointer(shader.attribute_vnrm, 3, VertexAttribPointerType.Float, true, 32, 2 * 4);
-			GL.VertexAttribPointer(shader.attribute_vpos, 3, VertexAttribPointerType.Float, false, 32, 5 * 4);
-
-			// bind triangle index data and render
-			GL.BindBuffer(BufferTarget.ElementArrayBuffer, me.triangleBufferId);
-			GL.DrawArrays(PrimitiveType.Triangles, 0, me.triangles.Length * 3);
-
-			// bind quad index data and render
-			if (me.quads.Length > 0)
-			{
-				GL.BindBuffer(BufferTarget.ElementArrayBuffer, me.quadBufferId);
-				GL.DrawArrays(PrimitiveType.Quads, 0, me.quads.Length * 4);
-			}
-
-			// restore previous OpenGL state
-			GL.UseProgram(0);
-			GL.PopClientAttrib();
-			
+			Matrix4 transform = Matrix4.CreateTranslation(pos) * Matrix4.CreateRotationY(rotation) * Matrix4.CreateScale(scale);
+			me.Render(me.shader, transform * camera, transform, me.texture);		
 
 			// call render function of the children here?
 			foreach (SceneGraph child in children)
             {
 				//child.me.modelViewMatrix *= me.modelViewMatrix; 
-				child.Render(camera);
+				child.Render(transform * camera);
             }
 		}
 
